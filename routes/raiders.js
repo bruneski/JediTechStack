922c8c0f069678cd555b19f8fa11a36c9f0b6845
Matthew Brune <bruneskidev@gmail.com>
2015-02-11 18:37:39 -0500

    Added some code to test raiders.js

--- routes/raiders.js
+++ routes/raiders.js
@@ -1,3 +1,16 @@
 /**
  * Created by mbrune on 2/10/2015.
  */
+
+var express = require('express');
+var router = express.Router();
+
+exports.findAll = function(req, res) {
+    res.send([{name:'raider1'}, {name:'raider2'}, {name:'raider3'}]);
+};
+
+exports.findById = function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
+};
+
+module.exports = router;
